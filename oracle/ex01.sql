3f9490e892a86cafc02a772b06634ad118ffa4aa
yhyuk <79249238+yhyuk@users.noreply.github.com>
2021-05-12 19:32:37 +0900

    Add files via upload

---
+++ oracle/ex01.sql
@@ -0,0 +1,287 @@
+-- ex01.sql
+
+--  단일라인 주석
+
+/*
+    다중라인주석
+*/
+
+/*
+    오라클, Oracle
+    - 회사명, 제품명
+    - 데이터베이스(Database) -> 데이터베이스 관리 시스템(Database Management System. DBMS)
+        -> 관계형 데이터베이스 관리 시스템(Relational DBMS, RDBMS)
+    - 프로젝트 하실때 DBMS 뭐쓰심? -> Oracle 11g
+    - 프로젝트 하실때 DB 클라이언트 뭐쓰심 ?  SQL Developer
+*/
+
+/*
+    다운로드
+    
+    1.OracleXE112_Win64
+    - 데이터베이스 프로그램(DBMS)
+    
+    2.sqldeveloper-20.4.1.407.0006-x64
+    - 데이터베이스 클라이언트 프로그램
+
+    데이터 Data
+    - 가공된 정보
+    
+    데이터베이스, Database
+    - 데이터의 집합
+    - 데이터의 집합을 지원하는 프로그램
+    
+    데이터베이스 관리 시스템, Database Management System
+    - 데이터베이스 + 추가작업 -> 통합 시스템
+    - 오라클(Oracle)이라고 한다.
+    
+    <관리 시스템의 중요성>
+    1. 데이터 무결성  
+        - 데이터에 오류가 있으면 안된다.
+        - 제약조건(Constraint)를 사용한다.
+    
+    2. 데이터 독립성
+        - 데이터베이스에 변화가 발생하더라도 관계된 응용 프로그램들은 영향을 받지 않는다.
+        
+    3. 보안
+        - 데이터베이스내의 데이터를 함부로 겁근 방지
+        - 소유주나 접근권한이 있는 사용자만 접근 가능, 통제 가능
+        
+    4. 데이터 중복 최소화
+        - 동일한 데이터가 여러곳에 여러번 저장되는 것을 방지한다.
+        
+    5. 데이터 안정성
+        - 데이터 백업/복원 기능을 제공한다.
+        
+    <DBMS 종류>
+    1. 계층형 DBMS
+    2. 망형 DBMS
+    3. 관계형 DBMS > 현재 가장많이 쓰이는 DBMS > 데이터를 '표' 형태로 저장/관리
+    4. 객체지향형 DBMS
+    5. 객체관계형 DBMS
+
+
+    관계형 DB 관리 시스템 > 제품 종류
+    1. Oracle
+        - Oracle
+        - 기업용 (개인용으로는 잘 안쓰임 > 비싸서...)
+        
+    2. MS-SQL
+        - Microsoft
+        - 기업용
+        
+    3. MySQL
+        - Oracle
+        - 개인용 + 기업용
+        
+    4. MariaDB
+        - MySQL 기반
+        - 무료
+        - 개인용 + 기업용 
+        
+    5. PostgreSQL
+        - 포스트그레스큐엘
+        - 무료
+        - 개인 + 기업용
+        
+    6. DB2
+        - IBM
+        - 메인프레임
+    
+    7. Access
+        - MS
+        - 개인용 + 소규모
+    
+    8. 티베로
+        - 티맥스(TMax)
+        
+    9. SQLite
+        - 경량
+        - 모바일 등..  
+    -------------------------------------------------------------------------------------------    
+    Oracle
+    - UI가 없는 프로그램(눈에 안보이는 프로그램, 서비스)    
+    
+    데이터베이스 클라이언트 프로그램
+    - UI가 없는 오라클에 접속을 해서 > 조작을 도와주는 툴
+      
+     1. SQL Developer
+        - 무료
+        - Oracle
+        
+    2. Toad
+        - 유료
+        - 점유율 최상
+        
+    3. SQLGate
+    
+    4. DataGrip(JetBrain)
+        - 30일 평가판 
+        - 대학교 이메일(무료)
+    
+    5. Eclipse
+    
+    6. SQL*Plus
+    - 오라클을 설치하면 자동으로 같이 설치되는 클라이언트 툴
+    - CLI(Command Line Interface)
+    ----------------------------------------------------------------------------------------------
+    
+    오라클 버전
+    - Oracle 11g Express Edition
+    - Oracle 1.0 ~ 21c
+    
+    오라클 에디션
+    - Express Edition
+        - 무료, 상용 가능(개발용), 기능제한
+        - 11g XE, 18c XE
+    - Enterprise Edition
+        -상용
+        
+        
+    오라클 설치
+    -> 오라클이 잘 동작하고 있는지?
+    -> 시작 or 종료?
+    
+    1. 실행창 > services.msc
+    2. Oracle로시작하는 단어
+        - OracleServiceXE 
+            - 데이터베이스 프로그램
+        - OracleXETNSListener
+            - 오라클과 클라이언트 프로그램을 연결시켜 주는 프로그램
+        - 위 2가지만 기본적으로 알고있자.
+        - 항상 실행중으로 있어야함
+        
+
+
+
+
+
+
+
+    열려있는 파일(ex01.sql , localhost.system) 2개
+    -> 스크립트 파일(*.sql), 워크 시트 파일(*.sql)
+    -> 오라클(데이터베이스 서버)와 대화를 하기위한 작업(자바로 따지면 소스파일(*java))
+    
+    스크립트 실행 방법
+    -> 실행할 명령어(문장)을 선택한다.
+    -> 실행: Ctrl + Enter
+    -> 스크립트는 문장 단위로 실행한다.
+    
+    계정
+    1. 관리자 계정 
+        - sys.system
+        
+    2. 일반 사용자 계정
+        - 권한이 일부 제한되어 있는 계정
+        - 생성(나중에)
+        - 학습용 계정 제공 > scott, hr(사용 금지 -> 해제)
+
+*/
+
+--계정이 잠겨있다. > 활성화
+--alter user 계정명 account unlock;
+alter user hr account unlock; --User HR이(가) 변경되었습니다.
+
+--비밀번호 변경
+alter user hr identified by java1234; --User HR이(가) 변경되었습니다.
+
+--위에 계정명, 비밀번호를 합친 한줄로 작성한 코드
+alter user hr account unlock identified by java1234;
+
+--내가 지금 system(관리자)인지, hr(일반 사용자)인지 구분하는법??
+
+--방법1 tabs 확인하기
+--system -> 154 line 오른쪽 상단 드럼통을 localhost.system을 hr로 변경 후 
+--hr -> 7 line
+select * from tabs;
+
+
+--방법2 사용자 확인하기
+show user;
+
+--결론: 오른쪽 상단 드럼통을 잘 확인합시다!! 실수 많이함!!
+
+
+
+--DB 서버 설치(오라클 서버) -> DB 클라이언트 툴(SQL Developer) > hr 계정활성화 > DB서버 접속
+
+/*
+
+    관계형 DB
+    - 데이터를 표형태로 저장/관리 한다.
+    - 데이터끼리의 관계를 관리한다.
+    - 표(테이블)의 집합
+    
+    localhost.hr
+    - 테이블
+        - countries
+        - departments
+        - employees
+    - 뷰
+    - 인덱스
+    - 패키지
+    - 프로시저
+    - 연산자
+    - 함수
+    - ...
+    
+    1. 테이블 
+    - 열(컬럼)의 집합 > 테이블의 구조 > 스키마(Schema)
+    
+    열, Column
+    - 컬럼, 필드(Field), 속성(Attribute), 특성(Property)
+    - 세부정보
+    
+    행, Row
+    - 행, 로우, 레코드(Record), 튜플(Tuple)
+    - 테이블의 실체화된 데이터 1건
+    - Object(객체)
+
+    SQL(오라클 기준)
+    1. ANSI SQL
+        -표준 SQL
+        - 명령어 들을 성격에 따라 분류
+    2. PL/SQL
+        - 자체 SQL
+        - 오라클에서만 동작되는 명령어
+        
+    
+    
+    
+    
+    1. DDL(데이터 정의어)
+        - Data Definition Language
+        - 구조를 만드는 언어
+        - 테이블, 뷰, 사용자, 인덱스 등의 객체(DB Object)를 생성, 수정, 삭제 하는 명령어
+        - DB 관리자, DB 담당자, 프로그래머(일부)만 사용.. 
+        - create(읽기), drop(삭제), alter(수정)
+    
+    2. DML(데이터 조작어)
+        - Data Manipulation Language
+        - 데이터베이스의 데이터를 추가, 수정, 삭제, 조회하는 명령어
+        - DB 관리자, DB 담당자, 프로그래머(일부)만 사용..
+        - select(읽기), insert(추가), update(수정), delete(삭제)
+    
+    3. DCL(데이터 제어어)
+        - Data Control Language
+        - 계정, 보안, 트랜잭션 등을 제어
+        - DB 관리자, DB 담당자, 프로그래머(일부)만 사용..
+        - commit, rollback, grant, revoke
+        
+    4. DQL(데이터 질의어)
+        - Data Query Language
+        - DML 중에 select만을 이렇게 따로 칭한다.
+        
+    5. TCL
+        - Transaction Control Language
+        - DCL 중에 commit, rollback만을 이렇게 따로 칭한다.
+
+
+*/
+
+--현재 계정(HR)이 소유하고 있는 테이블 목록을 보여주세요
+--SQL은 키워드의 대소문자를 구분하지 않는다.(보통 대문자로 많이쓴다.)
+select * from tabs;
+SELECT * FROM tabs;
+
+
---
+++ oracle/ex02_datatype.sql
@@ -0,0 +1,184 @@
+--ex02_datatype.sql
+
+/*
+
+    새로운 언어를 접할때, 자료형을 먼저 파악해보자..
+    
+    <ANSI-SQL 자료형>
+    - 오라클 자료형
+    - 데이터베이스 > 데이터 > 자료형
+    
+    1. 숫자형: 정수 + 실수(오라클은 숫자 구분이 없음)
+        - 표현방법: number
+        - 유효자리: 38자리 이하의 숫자를 표현하는 자료형
+        - 바이트: 5~22byte
+        - number(percision, scale)
+            1) precision : 소수 이하를 포함한 전체 자릿수(1~38)
+            2) scale: 소수 이하의 자릿수(0~127)
+            ex) number -> 38자리까지 표현 가능한 모든 숫자(정수,실수 포함)
+            ex) number(3) -> 3자리까지 표현 가능한 숫자(정수) -> -999 ~ +999
+            ex) number(4,2) -> 4자리수까지 표현 가능 숫자(실수) -> -99.99 ~ +99.99
+            ex) number(10,3) -> -9999999.999 ~ +9999999.999
+        - 숫자형 상수(리터럴)
+            1) 정수: 123
+            2) 실수: 3.14
+        - 오라클에도 byte, int, float형이 있긴있지만 사용 XXX
+            
+
+*/
+
+--테이블 삭제하기
+drop table tblType;
+
+--테이블 생성하기
+create table tblType (
+    -- 컬럼 구성
+    -- 컬럼명 자료형
+    --num number
+    --num number(3)
+    num number(4,2)
+);
+
+--테이블의 모든 내용을 가져오시오
+select * from tblType;
+
+--데이터 추가하기
+insert into tblType (num) values (100);
+insert into tblType (num) values (200);
+insert into tblType (num) values (300);
+insert into tblType (num) values (99.99);
+insert into tblType (num) values (3.14);
+insert into tblType (num) values (9999); --ORA-01438: value larger than specified precision allowed for this column
+insert into tblType (num) values (1234567.891233);
+insert into tblType (num) values (135135243231234567902891232132133);
+insert into tblType (num) values (-135135243231234567902891232132133);
+insert into tblType (num) values (135135243231234567902891232132131231232132133);
+
+
+/*
+
+    2. 문자형: 문자 + 문자열(오라클은 문자, 문자열 구분안함)
+        - 자바의 String과 유사
+        - 표현방법
+            - 쉽게 구분하는법
+            - n(X) vs n(O)
+            - var(X) vs var(O) -> var: 공간의 크기가 변한다.
+            1) char (************************)
+                - 고정 자릿수 문자열 -> 공간보다 데이터가 짧아도 나머지 공간은 그대로 차지한다. (공백으로)
+                - char(n): n자리 문자열, n(바이트)
+                    -최소 크기: 1byte
+                    -최대 크기: 2000byte
+                    ex) char -> X
+                    ex) char(3) -> 최대 3바이트까지 문자열을 저장 -> 인코딩? -> 영어(3글자), 한글(1글자)
+                    ex) char(10) -> 최대 10바이트까지 문자열을 저장 -> 영어(10글자), 한글(3글자);
+                    ex) char(2000) -> 영어(2000글자), 한글(666글자)
+            2) nchar
+                - n : national > 유니코드 지원 > UTF-16 동작(모든 문자 -> 2byte)
+                - nchar(n) : n자리 문자열, n(문자 수)
+                    -최소 크기: 1글자(2byte)
+                    -최대 크기: 1000글자(2000byte)
+                - 영어가 아닌 문자를 저장할 때 용이
+            
+            3) varchar2 (************************)
+                - 가변 자릿수 문자열 -> 데이터 크기만큼 공간의 크기가 줄어든다.
+                - varchar2(n) : n자리 문자열, n(바이트)
+                    -최소 크기: 1byte
+                    -최대 크기: 4000byte
+                
+            4) nvarchar2
+            
+            lob(Large Object)
+            5) clob, nclob
+                - 대용량 텍스트 저장 자료형
+                - 128TB
+                - 잘 사용 안함, 참조형
+                
+
+    3. 날짜/시간형
+        - 자바 Calendar, Date
+        1) date(******)
+            - 년월일시분초
+            - 크기: 7byte
+            - 기원전 4712년 1월 1일 ~ 9999년 12월 31일
+        2) timestamp
+            - 년월일시분초 + 밀리초(나노초)
+        3) interval
+            - 틱값
+            - 시간
+            - number 대신
+    
+    4. 이진 데이터 자료형
+        - 텍스트가 아닌 데이터
+        - 이미지, 영상, 음악 등...
+        1) blob
+            -128TB
+        - 잘안쓰인다....
+            
+        <나중에 실제 게시판 만들 때>
+        첨부 파일 -> 첨부 파일명(+경로) -> 문자열 -> varchar2
+
+
+
+
+    오늘 자료형 배운것들 요약하자면.....
+    number + varchar2 + date 이 3가지가 가장 많이 쓰인다.
+*/
+
+--char(n), varchar2(n) 예제
+drop table tblType;
+
+create table tblType (
+    --name char(3) --char 예제
+    --name char(10),
+    --nick varchar2(10) --varchar2 예제
+    
+    name char(10),
+    nick nchar(10) --nchar 예제
+);
+
+select * from tblType;
+
+--char(n)
+insert into tblType (name) values ('abc');
+insert into tblType (name) values ('abcd'); --value too large for column "HR"."TBLTYPE"."NAME" (actual: 4, maximum: 3)
+insert into tblType (name) values ('김');
+insert into tblType (name) values ('홍길동'); --(actual: 9, maximum: 3)
+insert into tblType (name) values ('123');
+insert into tblType (name) values ('&*(');
+
+--varchar2(n)
+insert into tblType (name, nick) values ('abc', 'abc');
+-- char     -> 'abc       ' -> 나머지 공간 추가되서 저장됨(불필요한 공간 생성)
+-- varchar2 -> 'abc'        -> 데이터 크기 만큼 줄어듬
+-- --> char(고정), varchar2(가변) 차이점
+
+--nchar(n)
+insert into tblType (name, nick) values ('홍길동', '홍길동');
+insert into tblType (name, nick) values ('홍길동님', '홍길동님');
+insert into tblType (name, nick) values ('홍길동', '홍길동님');
+insert into tblType (name, nick) values ('홍길동', '홍길동님안녕하세요호.');
+
+
+drop table tblType;
+
+create table tblType (
+
+    name varchar2(30),  --이름
+    age number(3),      --나이
+    birthday date       --생일
+
+);
+
+insert into tblType (name, age, birthday) values ('홍길동', 20, '1995-11-28');
+
+
+
+
+
+
+
+
+
+
+
+
